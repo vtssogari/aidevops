7fd15ae3921bdba4ef7036c2203d9ec52a6ef917
Kun Kang <Kun.Kang@fda.hhs.gov>
2021-06-15 10:52:10 -0400

    removing default password

--- nexus/scripts/create-image/configure.sh
+++ nexus/scripts/create-image/configure.sh
@@ -6,48 +6,73 @@ GROOVY_SCRIPT=/tmp/docker-script.json
 SCRIPT_NAME=dockerRepositories
 USER=admin
 DEFAULT_PASSWORD=admin123 # do not change - required for boot up
-PASSWORD=password123 # set this password to what you want it to be
+PASSWORD=$1 # set this password to what you want it to be
 
 # write out the Groovy script for configuring the Nexus repository manager
 # note the \n's are required for JSON payloads
 cat <<EOF > ${GROOVY_SCRIPT}
 {
 "name": "${SCRIPT_NAME}",
-"content": "import groovy.json.JsonOutput\n
-import org.sonatype.nexus.security.realm.RealmManager\n
-import org.sonatype.nexus.blobstore.api.BlobStoreManager\n
-def user = security.securitySystem.getUser('admin')\n
-user.setEmailAddress('admin@example.com')\n
-security.securitySystem.updateUser(user)\n
-security.securitySystem.changePassword('admin','${PASSWORD}')\n
-log.info('default password for admin changed')\n
-\n
-//enable Docker Bearer Token\n
-realmManager = container.lookup(RealmManager.class.name)\n
-realmManager.enableRealm('DockerToken')\n
-\n
-//Enable anonymois access which we above disabled\n
-security.anonymousAccess = true\n
-security.setAnonymousAccess(true)\n
-// create hosted repo and expose via https to allow deployments\n
-repository.createDockerHosted('docker-internal', null, null)\n
-\n
-// create proxy repo of Docker Hub and enable v1 to get search to work\n
-// no ports since access is only indirectly via group\n
-repository.createDockerProxy('docker-hub',                   // name\n
-                            'https://registry-1.docker.io', // remoteUrl\n
-                            'HUB',                          // indexType\n
-                            null,                           // indexUrl\n
-                            null,                           // httpPort\n
-                            null,                           // httpsPort\n
-                            BlobStoreManager.DEFAULT_BLOBSTORE_NAME, // blobStoreName\n
-                            true, // strictContentTypeValidation\n
-                            true)\n
-\n
-// create group and allow access via https\n
-def groupMembers = ['docker-hub', 'docker-internal']\n
-repository.createDockerGroup('docker-all', 8181, null, groupMembers, true, BlobStoreManager.DEFAULT_BLOBSTORE_NAME, false)\n
-log.info('Script dockerRepositories completed successfully')\n
+"content": "
+import groovy.json.JsonOutput \n
+import org.sonatype.nexus.security.realm.RealmManager \n
+import org.sonatype.nexus.blobstore.api.BlobStoreManager \n
+def user = security.securitySystem.getUser('admin') \n
+user.setEmailAddress('{{ EMAIL }}') \n
+security.securitySystem.updateUser(user) \n
+security.securitySystem.changePassword('admin','{{ PASSWORD }}') \n
+log.info('default password for admin changed') \n
+
+//enable Docker Bearer Token \n
+realmManager = container.lookup(RealmManager.class.name) \n
+realmManager.enableRealm('DockerToken') \n
+
+//Enable anonymois access which we above disabled \n
+security.anonymousAccess = true \n
+security.setAnonymousAccess(true) \n
+// create hosted repo and expose via https to allow deployments \n
+repository.createDockerHosted('docker-internal', null, null) \n
+
+// create proxy repo of Docker Hub and enable v1 to get search to work \n
+// no ports since access is only indirectly via group \n
+repository.createDockerProxy('docker-hub', 'https://registry-1.docker.io', 'HUB',                          // indexType \n
+                            null,                           // indexUrl \n
+                            null,                           // httpPort \n
+                            null,                           // httpsPort \n
+                            BlobStoreManager.DEFAULT_BLOBSTORE_NAME, // blobStoreName \n
+                            true, // strictContentTypeValidation \n
+                            true); \n
+
+// create group and allow access via https \n
+def groupMembers = ['docker-hub', 'docker-internal'] \n
+repository.createDockerGroup('docker-all', 8181, null, groupMembers, true, BlobStoreManager.DEFAULT_BLOBSTORE_NAME, false); \n
+log.info('Script dockerRepositories completed successfully'); \n
+
+// setup http server \n
+repository.createRawHosted('http-hosted', BlobStoreManager.DEFAULT_BLOBSTORE_NAME,  true,  WritePolicy.ALLOW); \n
+log.info('Script http-hosted completed successfully'); \n
+
+// setup pypi repo \n
+repository.createPyPiProxy('pypi-proxy', "https://pypi.org", BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createPyPiGroup('python-repo', ['pypi-proxy'], BlobStoreManager.DEFAULT_BLOBSTORE_NAME); \n
+log.info('Script python-repo completed successfully'); \n
+
+// setup Yum Proxy repos \n
+repository.createYumProxy('yum-centos', 'http://mirror.vtti.vt.edu/centos/7.9.2009/os/x86_64/', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createYumProxy('yum-centos-extras', 'http://mirror.es.its.nyu.edu/centos/7.9.2009/extras/x86_64/', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createYumProxy('yum-centos-updates', 'http://mirror.vtti.vt.edu/centos/7.9.2009/updates/x86_64/', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createYumProxy('yum-centosplus', 'http://mirror.cs.vt.edu/pub/CentOS/7.9.2009/centosplus/x86_64/', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createYumProxy('yum-cuda', 'https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createYumProxy('yum-docker-ce-stable', 'https://download.docker.com/linux/centos/7/x86_64/stable', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createYumProxy('yum-epel', 'https://dl.fedoraproject.org/pub/epel/7/x86_64/', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createYumProxy('yum-kubernetes', 'https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createYumProxy('yum-libnvidia-container', 'https://nvidia.github.io/libnvidia-container/centos7/x86_64', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createYumProxy('yum-nvidia-container-runtime', 'https://nvidia.github.io/nvidia-container-runtime/centos7/x86_64', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createYumProxy('yum-nvidia-dgx-7', 'https://international.download.nvidia.com/dgx/repos/rhel7/', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createYumProxy('yum-nvidia-dgx-7-updates', 'https://international.download.nvidia.com/dgx/repos/rhel7-updates/', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createYumProxy('yum-nvidia-docker', 'https://nvidia.github.io/nvidia-docker/centos7/x86_64', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n
+repository.createYumProxy('yum-nvidia-ml', 'https://developer.download.nvidia.com/compute/machine-learning/repos/rhel7/x86_64/', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true); \n 
+log.info('Script yum proxy repo completed successfully'); \n
 ",
 "type": "groovy"
 }
--- nexus/scripts/create-image/export.sh
+++
@@ -1,2 +0,0 @@
-docker commit -p nexus nexus_cached
-docker save -o nexus_cached.tar nexus_cached
